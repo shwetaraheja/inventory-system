a33798ab1f2c79cac5be741fd4176c9901078e9c
Your Name <shet9800@gmail.com>
2025-05-21 09:24:56 -0400

    file name change

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
 import dotenv from "dotenv";
-import Product from "./models/product.js";
+import Product from "./models/Product.js";
 
 dotenv.config();
 
